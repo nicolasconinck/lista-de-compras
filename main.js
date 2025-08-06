181d45656834838e38b23d4dc25422f25924a88c
Nicolas José Coninck <nicolas.coninck@remotatec.com.br>
2025-08-06 08:47:54 -0300

    Alteração para uso do local storage para armazenamento de dados.

--- main.js
+++ main.js
@@ -5,6 +5,18 @@ const form = document.getElementById("form-itens")
 const itensInput = document.getElementById("receber-item")
 const ulItens = document.getElementById("lista-de-itens")
 const ulItensComprados = document.getElementById("itens-comprados")
+const listaRecuperada = localStorage.getItem('listaDeItens')
+
+function atualizaLocalStorage() {
+    localStorage.setItem('listaDeItens', JSON.stringify(listaDeItens))
+}
+
+if(listaRecuperada){
+    listaDeItens = JSON.parse(listaRecuperada)
+    mostrarItem()
+} else {
+    listaDeItens = []
+}
 
 form.addEventListener("submit", function (evento) {
     evento.preventDefault()
@@ -20,11 +32,11 @@ function salvarItem() {
     if(checarDuplicado) {
         alert("Item já existe")
     } else {
-    listaDeItens.push({
-        valor: comprasItem,
-        checar: false
-    })
-}
+        listaDeItens.push({
+            valor: comprasItem,
+            checar: false
+        })
+    }
 
     itensInput.value = ''
 }
@@ -36,31 +48,30 @@ function mostrarItem(){
     listaDeItens.forEach((elemento, index) => {
         if(elemento.checar) {
         ulItensComprados.innerHTML += `
-    <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
-        <div>
-            <input type="checkbox" checked class="is-clickable" />  
-            <span class="itens-comprados is-size-5">${elemento.valor}</span>
-        </div>
-        <div>
-            <i class="fa-solid fa-trash is-clickable deletar"></i>
-        </div>
-    </li>
+            <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
+                <div>
+                    <input type="checkbox" checked class="is-clickable" />  
+                    <span class="itens-comprados is-size-5">${elemento.valor}</span>
+                </div>
+                <div>
+                    <i class="fa-solid fa-trash is-clickable deletar"></i>
+                </div>
+            </li>
         `
         } else {
-        ulItens.innerHTML += `
-    <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
-        <div>
-            <input type="checkbox" class="is-clickable" />
-            <input type="text" class="is-size-5" value="${elemento.valor}" ${index !== Number(itemAEditar) ? 'disabled' : ''}></input>
-        </div>
+            ulItens.innerHTML += `
+                <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
+                    <div>
+                        <input type="checkbox" class="is-clickable" />
+                        <input type="text" class="is-size-5" value="${elemento.valor}" ${index !== Number(itemAEditar) ? 'disabled' : ''}></input>
+                    </div>
 
-        <div>
-            ${ index === Number(itemAEditar) ? '<button onclick="salvarEdicao()"><i class="fa-regular fa-floppy-disk is-clickable"></i></button>' : '<i class="fa-regular is-clickable fa-pen-to-square editar"></i>'}
-            <i class="fa-solid fa-trash is-clickable deletar"></i>
-        </div>
-    </li>
-    `
-    }
+                    <div>
+                        ${ index === Number(itemAEditar) ? '<button onclick="salvarEdicao()"><i class="fa-regular fa-floppy-disk is-clickable"></i></button>' : '<i class="fa-regular is-clickable fa-pen-to-square editar"></i>'}
+                        <i class="fa-solid fa-trash is-clickable deletar"></i>
+                    </div>
+                </li>`
+        }
     })
 
     const inputsCheck = document.querySelectorAll('input[type="checkbox"]')
@@ -92,11 +103,11 @@ function mostrarItem(){
         })
     })  
 
+    atualizaLocalStorage()
 }
 
 function salvarEdicao() {
     const itemEditado = document.querySelector(`[data-value="${itemAEditar}"] input[type="text"]`)
-    // console.log(itemEditado.value)
     listaDeItens[itemAEditar].valor = itemEditado.value
     console.log(listaDeItens)
     itemAEditar = -1
